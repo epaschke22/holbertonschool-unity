18a776c3cedc025a999ebd14dd047efd57099ff1
epaschke22 <ericksonpaschke@gmail.com>
2021-09-21 15:35:59 -0400

    Inverting Y toggle implemented

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    public CinemachineFreeLook mainCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (OptionsMenu.isInverted == true)
+		{
+            mainCam.m_YAxis.m_InvertInput = true;
+        }
+        else
+		{
+            mainCam.m_YAxis.m_InvertInput = false;
+        }
     }
 
     // Update is called once per frame
--- 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -2,11 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+	public Toggle yToggle;
+	public static bool isInverted = true;
+
+	private void Start()
+	{
+		if (isInverted == true)
+		{
+			yToggle.isOn = true;
+		} 
+		else
+		{
+			yToggle.isOn = false;
+		}
+	}
+
 	public void Back()
 	{
 		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Apply()
+	{
+		if (yToggle.isOn == true)
+		{
+			isInverted = true;
+		}
+		else
+		{
+			isInverted = false;
+		}
 	}
 }
